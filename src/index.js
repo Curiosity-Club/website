9073002eb9f02ba3670c7dabf5f805a578e0e12b
jain-arham-hsr <jain.arham.hsr@gmail.com>
2021-10-16 22:14:01 +0530

    Modify cover design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 caret = document.getElementById("caret");
-hobbies_div = document.getElementById("hobbies");
+motto_div = document.getElementById("motto");
 
 const text = ["Connect.", "Learn.", "Innovate."];
 
@@ -14,9 +14,9 @@ typewriterAnim = (text) => {
 	i = window.i;
 	j = window.j;
 	if (i > 0) {
-		hobbies_div.innerHTML += text[j][i - 1];
+		motto_div.innerHTML += text[j][i - 1];
 	} else if (i <= 0) {
-		hobbies_div.innerHTML = hobbies_div.innerHTML.slice(0, -i);
+		motto_div.innerHTML = motto_div.innerHTML.slice(0, -i);
 	}
 	if (i > text[j].length - 1) {
 		window.i = -text[j].length;
@@ -25,7 +25,7 @@ typewriterAnim = (text) => {
 			refreshIntervalId = setInterval(typewriterAnim, 110, (s = text));
 		}, 530);
 	}
-	if (hobbies_div.innerHTML == "") {
+	if (motto_div.innerHTML == "") {
 		window.j += 1;
 	}
 	if (window.j >= text.length) {
